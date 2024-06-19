9ea1111b80194b7c1ac4bebd8e49232fcb8d30a6
vijaykumar-1012 <vijaykumarelakala@gmail.com>
2024-06-19 19:12:00 +0530

    Add files via upload

---
+++ TASK 3/script.js
@@ -0,0 +1,23 @@
+const display =document.querySelector(".display");
+const buttons =document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output ="";
+ const calculate =(btnvalue) => 
+    {
+    if (btnvalue === "=" && btnvalue !== "") {
+        output = eval(output.replace("%", "/100"));
+    } else if(btnvalue === "AC"){
+        output = "";
+     }else if (btnvalue === "DEL") { 
+        output = output.toString().slice(0, -1);
+    } else {
+        if(output === "" && specialChars.includes(btnvalue)) return;
+        output += btnvalue;
+    }
+    display.value = output;
+    };
+buttons.forEach((button) => {
+
+button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+
+});
